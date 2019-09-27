b4f6364a2c7234c7aaaf4cc957f8d299e0f0aa29
Sidharth J <sidharthjayaprakash93@gmail.com>
2019-09-27 00:06:12 -0700

    Modified signout flow

--- grubhub/app/src/routes.js
+++ grubhub/app/src/routes.js
@@ -12,7 +12,6 @@ import Search from './components/Search/Search';
 import SearchResults from './components/Search/SearchResults';
 import Restaurant from './components/Restaurant/Restaurant';
 import Cart from './components/Cart/Cart';
-import Unauthorized from './components/Signout/Signout';
 
 const Routes = () => {
   return (
@@ -30,7 +29,6 @@ const Routes = () => {
       <Route exact path="/searchresults" component={SearchResults} />
       <Route exact path="/restaurant/detail/:restaurant_id" component={Restaurant} />
       <Route exact path="/:id/cart" component={Cart} />
-      <Route exact path="/unauthorize" component={Unauthorized}/>
     </div>
   );
 };
